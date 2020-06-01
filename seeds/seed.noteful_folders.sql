d5bbe8c751cf42846c96fd48cbd4c7c917351d56
Brendan Reape <brendanreape88@gmail.com>
2020-06-01 13:41:40 -0700

    first commit

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_folders (id, folder_name)
+VALUES
+    (1, 'First Folder'),
+    (2, 'Second Folder'),
+    (3, 'Third Folder'),
+    (4, 'Fourth Folder'),
+    (5, 'Fifth Folder')
